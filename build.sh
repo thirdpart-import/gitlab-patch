a747065298fd36e3811eedab8f87450682c68bf5
yhl <yanghailong@zwcad.com>
2018-12-19 11:25:53 +0800

    更新脚本

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ git clone https://gitlab.com/xhang/gitlab.git gitlabce
 
 cd gitlabce
 
-git diff v${gitlab_version} v${gitlab_version}-zh > ../patch.diff
+IGNORE_DIRS=':!qa :!spec :!features :!.gitignore :!.gitlab :!locale :!app/assets/ :!vendor/assets/'
+git diff --diff-filter=d v${gitlab_version} v${gitlab_version}-zh -- . ${IGNORE_DIRS} > ../patch.diff
 
 #git checkout v${gitlab_version}-zh
 
